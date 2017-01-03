3ce043740a5470eac252b4eb2a6c037019ba6609
Nuno Azevedo <nunoaz95@gmail.com>
2017-01-03 16:21:42 +0000

    Add new line before message
    Signed-off-by: Nuno Azevedo <nunoaz95@gmail.com>

--- src/main/java/NotifierI.java
+++ src/main/java/NotifierI.java
@@ -1,5 +1,5 @@
 public class NotifierI extends Streaming._NotifierDisp {
-    public void inform(String stream, Ice.Current current) {
-        System.out.print(stream + "\nstream: ");
+    public void inform(String message, Ice.Current current) {
+        System.out.print("\n" + message + "\nstream: ");
     }
 }
