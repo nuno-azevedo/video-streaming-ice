99a17315ce9830d9a56e8c2b557d9b309e1dd3b0
Nuno Azevedo <nunoaz95@gmail.com>
2017-01-03 16:25:12 +0000

    Error checking on register
    Signed-off-by: Nuno Azevedo <nunoaz95@gmail.com>

--- src/main/java/Streamer.java
+++ src/main/java/Streamer.java
@@ -4,26 +4,34 @@ import javax.swing.*;
 
 public class Streamer {
     public static void main(String args[]) {
+        if (args.length != 1) {
+            System.err.println("USAGE: java Streamer $PORTAL_PORT");
+            System.exit(1);
+        }
+
         int status = 0;
         Ice.Communicator ic = null;
         try {
             ic = Ice.Util.initialize(args);
-            Ice.ObjectPrx base = ic.stringToProxy("Portal: default -p 11000");
+            Ice.ObjectPrx base = ic.stringToProxy("Portal: default -p " + args[0]);
             PortalPrx portal = PortalPrxHelper.checkedCast(base);
             if (portal == null) throw new Error("Invalid proxy");
 
             Stream stream = new Stream(
                 "The Vagabond",
-                new Endpoint("tcp", "127.0.0.1", 10000),
+                new Endpoint("tcp", "127.0.0.1", 12000),
                 new Resolution(480, 720),
                 400,
                 new String[] { "Film", "Story", "Vagabond" }
             );
-            portal.register(stream);
+            if (!portal.register(stream)) {
+                System.err.println("register: stream already exists");
+                System.exit(1);
+            }
 
-            new Thread(() -> new Timer(60000, (z) -> portal.update(stream)).start()).start();
-
+            new Thread(() -> new Timer(5000, (z) -> portal.update(stream)).start()).start();
             while (true);
+//            portal.remove(stream);
         } catch (Ice.LocalException e) {
             e.printStackTrace();
             status = 1;
